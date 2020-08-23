cfc46dbee78798ed33ceaf9e2c61ddf5dedac9ec
xiguanlezz <41047090+xiguanlezz@users.noreply.github.com>
2020-08-23 21:05:48 +0800

    change port
    修改webpack部署的端口为本地80端口

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -86,7 +86,7 @@ var config = {
 };
 
 if('dev' === WEBPACK_ENV){
-    config.entry.common.push('webpack-dev-server/client?http://localhost:8088/');
+    config.entry.common.push('webpack-dev-server/client?http://localhost:80/');
 }
 
 module.exports = config;
